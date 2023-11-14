93ec9b1c6facdaf1394463374d95d5de5b968249
Sasha <spoor@vassar.edu>
2023-11-14 17:27:48 -0500

    Added system test checking multiple dates

--- intellij/astudio/app/src/androidTest/java/edu/vassar/cmpu203/app/ViewDayTest.java
+++ intellij/astudio/app/src/androidTest/java/edu/vassar/cmpu203/app/ViewDayTest.java
@@ -2,6 +2,7 @@ package edu.vassar.cmpu203.app;
 
 import android.content.Context;
 import android.os.SystemClock;
+import android.util.Log;
 
 import androidx.test.espresso.*;
 import androidx.test.espresso.action.ViewActions;
@@ -35,6 +36,7 @@ public class ViewDayTest {
         dateInput.check(ViewAssertions.matches(ViewMatchers.withText(LocalDate.now().toString())));
 
         //Enter desired date
+        dateInput.perform(ViewActions.clearText());
         dateInput.perform(ViewActions.typeText("2022-04-27"));
         Espresso.closeSoftKeyboard();
 
@@ -44,13 +46,18 @@ public class ViewDayTest {
         SystemClock.sleep(30000); //Wait to fetch from website
 
         //Check that certain meals are in the menu
+        ViewInteraction menuTitle = Espresso.onView(ViewMatchers.withId(R.id.menuTitle));
+        menuTitle.check(ViewAssertions.matches(Matchers.allOf(ViewMatchers.withText("2022-04-27"),ViewMatchers.withId(R.id.menuTitle))));
         ViewInteraction menuView = Espresso.onView(ViewMatchers.withId(R.id.menusDisplay));
-        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Vegan White Bean and Chickpea Soup")));
+        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("@The Farmer")));
+        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Vegan White Bean And Chickpea Soup")));
         menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Pesto Shrimp Pizza")));
         menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Lemon-garlic Pasta Primavera")));
-        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Corn and Black Bean Cheese Quesadillas")));
+        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Corn And Black Bean Cheese Quesadillas")));
 
         //Enter new date
+        dateInput.perform(ViewActions.clearText());
+
         dateInput.perform(ViewActions.typeText("2023-11-11"));
         Espresso.closeSoftKeyboard();
 
@@ -59,27 +66,28 @@ public class ViewDayTest {
         SystemClock.sleep(30000); //Wait to fetch from website
 
         //Check that certain meals are in the menu
-        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Stuffed French Toast with Berries and Cream Cheese")));
+        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Stuffed French Toast With Berries And Cream Cheese")));
         menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Hawaiian Style Pizza")));
-        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Spicy Thai Coconut Chicken Soup with Noodles")));
-        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Mozzarella Stix with Marinara")));
+        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Spicy Thai Coconut Chicken Soup With Noodles")));
+        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Mozzarella Stix With Marinara")));
 
         //Check that old menu items are no longer displayed
         menuView.check(ViewAssertions.matches(Matchers.not(ViewMatchers.withSubstring("Pesto Shrimp Pizza"))));
-        menuView.check(ViewAssertions.matches(Matchers.not(ViewMatchers.withSubstring("Vegan White Bean and Chickpea Soup"))));
+        menuView.check(ViewAssertions.matches(Matchers.not(ViewMatchers.withSubstring("Vegan White Bean And Chickpea Soup"))));
         menuView.check(ViewAssertions.matches(Matchers.not(ViewMatchers.withSubstring("Lemon-garlic Pasta Primavera"))));
-        menuView.check(ViewAssertions.matches(Matchers.not(ViewMatchers.withSubstring("Corn and Black Bean Cheese Quesadillas"))));
+        menuView.check(ViewAssertions.matches(Matchers.not(ViewMatchers.withSubstring("Corn And Black Bean Cheese Quesadillas"))));
 
         //Try previous date again to check that it is not fetched again
+        dateInput.perform(ViewActions.clearText());
         dateInput.perform(ViewActions.typeText("2022-04-27"));
         Espresso.closeSoftKeyboard();
         dateButton.perform(ViewActions.click());
 
         //Check that certain meals are in the menu
-        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Vegan White Bean and Chickpea Soup")));
+        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Vegan White Bean And Chickpea Soup")));
         menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Pesto Shrimp Pizza")));
         menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Lemon-garlic Pasta Primavera")));
-        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Corn and Black Bean Cheese Quesadillas")));
+        menuView.check(ViewAssertions.matches(ViewMatchers.withSubstring("Corn And Black Bean Cheese Quesadillas")));
 
     }
 
